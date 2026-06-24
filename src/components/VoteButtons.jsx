058f91e144475fa6c78850d525be991ca0f43a18
k69191 <timo.brand@study.thws.de>
2026-06-24 10:56:20 +0200

    vote buttons can change state now

--- src/components/VoteButtons.jsx
+++ src/components/VoteButtons.jsx
@@ -1,12 +1,25 @@
+import {useState} from "react";
+
 export default function VoteButtons({ up, down }) {
+    const [upVotes, setUpVotes] = useState(up);
+    const [downVotes, setDownVotes] = useState(down);
+
+    const voteUp = () => {
+        setUpVotes(upVotes + 1);
+    };
+
+    const voteDown = () => {
+        setDownVotes(downVotes + 1);
+    };
+
   return (
     <div className="note-votes">
-      <span className="vote-btn vote-up">
-        👍 <span className="vote-count">{up}</span>
-      </span>
-      <span className="vote-btn vote-down">
-        👎 <span className="vote-count">{down}</span>
-      </span>
+      <button className="vote-btn vote-up" onClick={voteUp}>
+        👍 <span className="vote-count">{upVotes}</span>
+      </button>
+      <button className="vote-btn vote-down" onClick={voteDown}>
+        👎 <span className="vote-count">{downVotes}</span>
+      </button>
     </div>
   );
 }
